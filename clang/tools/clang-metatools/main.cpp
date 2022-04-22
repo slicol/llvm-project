b811c8d111a734ac75200ac09debcfdd648811d5
slicol <slicol@qq.com>
2022-04-22 21:57:27 +0800

    AST解析生成DSL

--- clang/tools/clang-metatools/main.cpp
+++ clang/tools/clang-metatools/main.cpp
@@ -10,6 +10,7 @@
 // Declares llvm::cl::extrahelp.
 #include "llvm/Support/CommandLine.h"
 #include "clang/AST/Type.h"
+#include <stdarg.h>
 
 using namespace clang::tooling;
 using namespace llvm;
@@ -17,6 +18,103 @@ using namespace llvm;
 //////////////////////////////////////////////////////////////////////////
 using namespace clang;
 
+#define mtstr std::string
+#define mtsize std::size_t
+
+bool MTStringStartWith(const mtstr& s, const mtstr& t)
+{
+	return s.substr(0, t.size()).compare(t) == 0;
+}
+
+
+mtstr& MTStringFormat(mtstr& buff, const char* fmt_str, ...)
+{
+	mtsize n = 256;
+
+	if (buff.size() < n)
+	{
+		buff.resize(n);
+	}
+	else
+	{
+		n = buff.size();
+	}
+
+	while (1)
+	{
+		va_list ap;
+		va_start(ap, fmt_str);
+		const int final_n = vsnprintf(&buff[0], n, fmt_str, ap);
+		va_end(ap);
+		if (final_n < 0)
+		{
+			n += static_cast<size_t>(-final_n);
+			buff = "encoding error";
+			break;
+		}
+
+		if (static_cast<size_t>(final_n) >= n)
+		{
+			n += static_cast<size_t>(final_n) - n + 1;
+			buff.resize(n);
+		}
+		else
+		{
+			buff[final_n] = '\0';
+			buff.resize(final_n);
+			break;
+		}
+	}
+	return buff;
+}
+
+static std::vector<mtstr>* MTBuiltinTypeTable;
+static void MTCheckBuiltinTypeTable()
+{
+	if (!MTBuiltinTypeTable)
+	{
+		MTBuiltinTypeTable = new std::vector<mtstr>((mtsize)BuiltinType::LastKind + 1);
+
+		(*MTBuiltinTypeTable)[BuiltinType::Void] = "void";
+		(*MTBuiltinTypeTable)[BuiltinType::NullPtr] = "void*";
+		(*MTBuiltinTypeTable)[BuiltinType::Bool] = "bool";
+		(*MTBuiltinTypeTable)[BuiltinType::Char_U] = "i8";
+		(*MTBuiltinTypeTable)[BuiltinType::Char_S] = "i8";
+		(*MTBuiltinTypeTable)[BuiltinType::UChar] = "i8";
+		(*MTBuiltinTypeTable)[BuiltinType::SChar] = "i8";
+		(*MTBuiltinTypeTable)[BuiltinType::Char8] = "i8";
+		(*MTBuiltinTypeTable)[BuiltinType::Short] = "i16";
+		(*MTBuiltinTypeTable)[BuiltinType::UShort] = "i16";
+		(*MTBuiltinTypeTable)[BuiltinType::Char16] = "i16";
+		(*MTBuiltinTypeTable)[BuiltinType::Char32] = "i32";
+		(*MTBuiltinTypeTable)[BuiltinType::Int] = "i32";
+		(*MTBuiltinTypeTable)[BuiltinType::UInt] = "i32";
+		(*MTBuiltinTypeTable)[BuiltinType::Long] = "i32";
+		(*MTBuiltinTypeTable)[BuiltinType::ULong] = "i32";
+		(*MTBuiltinTypeTable)[BuiltinType::LongLong] = "i64";
+		(*MTBuiltinTypeTable)[BuiltinType::ULongLong] = "i64";
+		(*MTBuiltinTypeTable)[BuiltinType::Float] = "float";
+		(*MTBuiltinTypeTable)[BuiltinType::Double] = "double";
+	}
+}
+
+void MTStringReplaceAll(mtstr& str, const mtstr& from, const mtstr& to)
+{
+	if (from.empty())
+		return;
+	mtsize start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != mtstr::npos)
+	{
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+	}
+}
+
+
+void MTNormalizeQualifiedName(mtstr& name)
+{
+	MTStringReplaceAll(name, "::", ".");
+}
 
 
 class FindNamedClassVisitor : public RecursiveASTVisitor<FindNamedClassVisitor>
@@ -24,46 +122,216 @@ class FindNamedClassVisitor : public RecursiveASTVisitor<FindNamedClassVisitor>
 public:
 	explicit FindNamedClassVisitor(ASTContext* Context) : Context(Context) {}
 
+	static bool MTCheckFilter(CXXRecordDecl* Decl)
+	{
+		bool bResult = true;
+		StringRef name = Decl->getName();
+		bResult = bResult && !name.empty();
+		bResult = bResult && !name.startswith("__vcrt");
+		bResult = bResult && !name.startswith("__crt");
+		bResult = bResult && !name.startswith("_Crt");
+		bResult = bResult && !name.startswith("_");
+		
+		std::string qname = Decl->getQualifiedNameAsString();
+		bResult = bResult && !MTStringStartWith(qname,"std::");
+		
+		bResult = bResult && !Decl->field_empty();
+
+		return bResult;
+	}
+
+
+	mtstr MTGetTypeName(const clang::Type* Ty)
+	{
+		MTCheckBuiltinTypeTable();
+
+		const clang::Type* DesugaredType = Ty->getUnqualifiedDesugaredType();
+
+		if (DesugaredType->isBuiltinType())
+		{
+			const BuiltinType* BT = DesugaredType->getAs<BuiltinType>();
+
+			int k = (int)BT->getKind();
+			mtstr name = (*MTBuiltinTypeTable)[k];
+			if (!name.empty())
+			{
+				return name;
+			}
+			else
+			{
+				return mtstr("(unkown)") + BT->getNameAsCString(Context->getPrintingPolicy());
+			}
+		}
+		else if (DesugaredType->isUnionType())
+		{
+			const RecordType* RT = DesugaredType->getAs<RecordType>();
+			const RecordDecl* RD = RT->getDecl();
+			mtstr name = RD->getQualifiedNameAsString();
+			MTNormalizeQualifiedName(name);
+			return mtstr("%") + name;
+		}
+		else if (DesugaredType->isStructureOrClassType())
+		{
+			const RecordType* RT = DesugaredType->getAs<RecordType>();
+			const RecordDecl* RD = RT->getDecl();
+			const ClassTemplateSpecializationDecl* CTSD = dyn_cast<ClassTemplateSpecializationDecl>(RD);
+			if (CTSD)
+			{
+				mtstr TmplArgList;
+				ArrayRef<TemplateArgument>  ArgList = CTSD->getTemplateInstantiationArgs().asArray();
+				for (int i = 0; i < ArgList.size(); ++i)
+				{
+					const TemplateArgument& Arg = ArgList[i];
+					QualType TAT = Arg.getAsType();
+					if (i == 0)
+					{
+						TmplArgList = MTGetTypeName(TAT.getTypePtr());
+					}
+					else
+					{
+						TmplArgList = TmplArgList + "," + MTGetTypeName(TAT.getTypePtr());
+					}
+				}
+
+				mtstr name = RD->getQualifiedNameAsString();
+				MTNormalizeQualifiedName(name);
+				return mtstr("%") + name + "<" + TmplArgList + ">";
+			}
+			else
+			{
+				mtstr name = RD->getQualifiedNameAsString();
+				MTNormalizeQualifiedName(name);
+				return mtstr("%") + name;
+			}
+		}
+		else if (DesugaredType->isEnumeralType())
+		{
+			const EnumType* ET = DesugaredType->getAs<EnumType>();
+			const EnumDecl* ED = ET->getDecl();
+			mtstr name = ED->getQualifiedNameAsString();
+			MTNormalizeQualifiedName(name);
+			return mtstr("%") + name;
+		}
+		else if (DesugaredType->isConstantArrayType())
+		{
+			const ConstantArrayType* CAT = dyn_cast<ConstantArrayType>(DesugaredType->getAsArrayTypeUnsafe());
+			
+			mtstr elt = MTGetTypeName(CAT->getElementType().getTypePtr());
+			mtstr n =  CAT->getSize().toString(10, false);
+			return mtstr("[") + n + " x " + elt + "]";
+		}
+		else if (DesugaredType->isVoidType())
+		{
+			return "void";
+		}
+		else  if (DesugaredType->isVoidPointerType())
+		{
+			return "void*";
+		}
+		else if(!DesugaredType->getPointeeType().isNull())
+		{
+			mtstr PtrTokens = "*";
+
+			QualType PTE = DesugaredType->getPointeeType();
+			while (!PTE->getPointeeType().isNull())
+			{
+				PtrTokens.append("*");
+				PTE = PTE->getPointeeType();
+			}
+
+			mtstr Pte = MTGetTypeName(PTE.getTypePtr());
+			mtstr name = Pte + PtrTokens;
+			return name;
+		}
+		else
+		{
+			
+			const TagDecl* TD = DesugaredType->getAsTagDecl();
+			if (TD)
+			{
+				mtstr name = TD->getQualifiedNameAsString();
+				MTNormalizeQualifiedName(name);
+				return mtstr("(unkown)") + name;
+			}
+		}
+		
+		return "(unkown)";
+	}
+
+
 	bool VisitCXXRecordDecl(CXXRecordDecl* Declaration)
 	{
-		llvm::outs() << *Declaration << "\n";
-		llvm::outs() << Declaration->getName() << "\n";
-		llvm::outs() << Declaration->getQualifiedNameAsString() << "\n";
+		if (!MTCheckFilter(Declaration))
+		{
+			return true;
+		}
 
-		std::string ScalarTypeNames[10];
-		ScalarTypeNames[clang::Type::STK_CPointer] = "STK_CPointer";
-		ScalarTypeNames[clang::Type::STK_BlockPointer] = "STK_BlockPointer";
-		ScalarTypeNames[clang::Type::STK_ObjCObjectPointer] = "STK_ObjCObjectPointer";
-		ScalarTypeNames[clang::Type::STK_MemberPointer] = "STK_MemberPointer";
-		ScalarTypeNames[clang::Type::STK_Bool] = "STK_Bool";
-		ScalarTypeNames[clang::Type::STK_Integral] = "STK_Integral";
-		ScalarTypeNames[clang::Type::STK_Floating] = "STK_Floating";
-		ScalarTypeNames[clang::Type::STK_IntegralComplex] = "STK_IntegralComplex";
-		ScalarTypeNames[clang::Type::STK_FloatingComplex] = "STK_FloatingComplex";
-		ScalarTypeNames[clang::Type::STK_FixedPoint] = "STK_FixedPoint";
+		mtstr structName = Declaration->getQualifiedNameAsString();
+		MTNormalizeQualifiedName(structName);
+		llvm::outs() << structName;
+		if (Declaration->getDescribedTemplate())
+		{
+			llvm::outs() << "<getDescribedTemplate>";
+		}
+		if (Declaration->getDescribedClassTemplate())
+		{
+			llvm::outs() << "<getDescribedClassTemplate>";
+		}
+		if (Declaration->getDescribedTemplateParams())
+		{
+			llvm::outs() << "<getDescribedTemplateParams>";
+		}
+		if (Declaration->getTemplateInstantiationPattern())
+		{
+			llvm::outs() << "<getTemplateInstantiationPattern>";
+		}
+		if (Declaration->isTemplated())
+		{
+			llvm::outs() << "<isTemplated>";
+		}
+		if (Declaration->isTemplateParameter())
+		{
+			llvm::outs() << "<isTemplateParameter>";
+		}
+		llvm::outs() << " = \n{\n";
 
 		for(const FieldDecl* field: Declaration->fields())
 		{
-			QualType fieldType = field->getType();
-			
-			
-			fieldType->dump();
-
-			llvm::outs() << "\t" << *field << ":" << fieldType->getUnqualifiedDesugaredType()->getTypeClassName() << ":";
-			if (fieldType->isScalarType())
-			{	
-				fieldType->getTypeClass();
-				llvm::outs() << ScalarTypeNames[(int)fieldType->getScalarTypeKind()];
-			}
-			else if (fieldType->isStructureOrClassType() || fieldType->isUnionType())
+			if (field->isTemplated())
 			{
-				const CXXRecordDecl* field_decl = fieldType->getAsCXXRecordDecl();
-				llvm::outs() << field_decl->getQualifiedNameAsString();
+				llvm::outs() << "<isTemplated>";
 			}
-			
-			llvm::outs() << "\n";
-		}
+			if (field->isTemplateDecl())
+			{
+				llvm::outs() << "<isTemplateDecl>";
+			}
 
+			QualType fieldType = field->getType();
+			const clang::Type* DesugaredType = fieldType->getUnqualifiedDesugaredType();
+			
+			llvm::outs() << "    " << *field << ":";
+
+			mtstr fieldTypeName = MTGetTypeName(DesugaredType);
+
+			llvm::outs() << fieldTypeName;
+
+			if (DesugaredType->isStructureOrClassType())
+			{
+				const RecordType* RT = DesugaredType->getAs<RecordType>();
+				const RecordDecl* RD = RT->getDecl();
+				
+			
+				if (RD->isTemplateDecl())
+				{
+					llvm::outs() << "<isTemplateDecl>";
+				}
+			}
+
+			//fieldType->getUnqualifiedDesugaredType()->dump(llvm::outs(), *Context);
+			
+			llvm::outs() << ",\n";
+		}
+		llvm::outs() << "};\n";
 		llvm::outs() << "--------------------------------------\n";
 
 		if (Declaration->getQualifiedNameAsString() == "n::m::C")
